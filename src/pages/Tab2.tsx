d2ed84e92a6d86119b9f085de2dc5569643cd798
Marianne Gil Estrada <20211076@nbsc.edu.ph>
2024-03-28 20:15:10 +0800

    Major changes, Added home.tsx and css, Profile, click-counter, and calculator  for this commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +17,12 @@ const Tab2: React.FC = () => {
           <IonTitle>Click Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Click Counter</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Click Counter" />
+      <IonContent fullscreen className="counter-content">
+        <div className="counter-container">
+          <h1>Click Counter</h1>
+          <p>Total Clicks: {count}</p>
+          <IonButton onClick={incrementCounter}>Click me!</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
