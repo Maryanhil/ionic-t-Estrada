f72c05ffe0ec9d97bf195cf33cc40b356c7e8ef0
Maryahil <20211076@nbsc.edu.ph>
2024-05-03 23:11:02 +0800

    added ToDoList tsx and css file, in ToDoList.tsx impoted Firestore functions and Firebase connection, Fetch todos from Firestore on component mount, and Add new todo to Firestore,

--- src/pages/ToDoList.tsx
+++ src/pages/ToDoList.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import { IonRouterLink, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCheckbox, IonInput, IonButton, IonIcon } from '@ionic/react';
-import { trashOutline, arrowBackCircle } from 'ionicons/icons'; // Added IonIcon import for delete icon
+import React, { useState, useEffect } from 'react';
+import { IonFooter, IonRouterLink, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCheckbox, IonInput, IonButton, IonIcon } from '@ionic/react';
+import { logoInstagram, logoTwitter, logoFacebook, trashOutline, arrowBackCircle } from 'ionicons/icons'; // Added IonIcon import for delete icon
+import './ToDoList.css';
+import { collection, addDoc, getDocs, deleteDoc, updateDoc, doc } from 'firebase/firestore'; // Import Firestore functions
+import { db } from './firebase'; // Import Firebase connection
 
 interface ToDoItem {
-  id: number;
+  id: string;
   text: string;
   completed: boolean;
 }
@@ -12,43 +15,63 @@ const ToDoList: React.FC = () => {
   const [todos, setTodos] = useState<ToDoItem[]>([]);
   const [newTodoText, setNewTodoText] = useState<string>('');
 
-  const handleAddTodo = () => {
+  useEffect(() => {
+    // Fetch todos from Firestore on component mount
+    const fetchTodos = async () => {
+      const todosSnapshot = await getDocs(collection(db, 'todos'));
+      const todosData = todosSnapshot.docs
+        .filter(doc => doc.id && doc.data().text) // Filter out docs without id or text
+        .map(doc => ({ id: doc.id, ...doc.data() } as ToDoItem)); // Type assertion here
+      setTodos(todosData);
+    };
+
+    fetchTodos();
+  }, []);
+
+  const handleAddTodo = async () => {
     if (newTodoText.trim() !== '') {
-      const newTodo: ToDoItem = {
-        id: Date.now(),
+      const newTodo: Omit<ToDoItem, 'id'> = {
         text: newTodoText,
         completed: false,
       };
-      setTodos([...todos, newTodo]);
+
+      // Add new todo to Firestore
+      const docRef = await addDoc(collection(db, 'todos'), newTodo);
+      setTodos([...todos, { ...newTodo, id: docRef.id }]);
       setNewTodoText('');
     }
   };
 
-  const handleToggleTodo = (id: number) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
+  const handleToggleTodo = async (id: string) => {
+    const todoToUpdate = todos.find(todo => todo.id === id);
+    if (todoToUpdate) {
+      const updatedTodo: Partial<ToDoItem> = { ...todoToUpdate, completed: !todoToUpdate.completed };
+      await updateDoc(doc(db, 'todos', id), updatedTodo);
+      const updatedTodos = todos.map(todo => (todo.id === id ? updatedTodo : todo));
+    }
   };
 
-  const handleDeleteTodo = (id: number) => {
+  const handleDeleteTodo = async (id: string) => {
+    await deleteDoc(doc(db, 'todos', id));
     const updatedTodos = todos.filter(todo => todo.id !== id);
     setTodos(updatedTodos);
   };
 
   return (
-
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>ToDo List</IonTitle>
         </IonToolbar>
       </IonHeader>
-
-      <IonRouterLink href="/Home">
+      <IonContent>
+        <IonRouterLink href="/Home">
             <IonIcon id="arrow" aria-hidden="true" icon={arrowBackCircle} />
                 </IonRouterLink>
-      <IonContent>
+        <IonItem>
+          <IonInput placeholder="Enter a new todo" value={newTodoText} onIonChange={(e) => setNewTodoText(e.detail.value!)} />
+          <IonButton slot="end" onClick={handleAddTodo}>Add</IonButton>
+        </IonItem>
         <IonList>
           {todos.map(todo => (
             <IonItem key={todo.id}>
@@ -60,10 +83,6 @@ const ToDoList: React.FC = () => {
             </IonItem>
           ))}
         </IonList>
-        <IonItem>
-          <IonInput placeholder="Enter a new todo" value={newTodoText} onIonChange={(e) => setNewTodoText(e.detail.value!)} />
-          <IonButton slot="end" onClick={handleAddTodo}>Add</IonButton>
-        </IonItem>
       </IonContent>
     </IonPage>
   );
